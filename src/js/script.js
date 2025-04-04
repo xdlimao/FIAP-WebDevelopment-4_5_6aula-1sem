f5b636450e6483618201994e2fe2d5e85c6bd06e
Daniel <danieloliveiradesouza09@outlook.com>
2025-04-04 11:07:44 -0300

    add files

---
+++ src/js/script.js
@@ -0,0 +1,89 @@
+//DECLARARAÇÕES E VARIAVEIS
+
+var nome ="fiap";
+console.log(nome);
+
+let idade = 19;
+console.log(idade)
+
+const sobrenome="Fiapinho";
+console.log(sobrenome)
+
+//declarando uma variavel indefinida
+let nome1;
+console.log(nome1)
+
+//declarando uma variavel nula
+let nome2=null;
+console.log(nome2)
+
+//TIPOS DE VARIAVEIS
+
+let exemplo1 =10;
+console.log(typeof exemplo1)
+
+let exemplo2 ="Tecnologia";
+console.log(typeof exemplo2)
+
+let exemplo3 =true;
+console.log(typeof exemplo3)
+
+let exemplo4={};
+console.log(typeof exemplo4)
+
+let exemplo5 =[];
+console.log(typeof exemplo5)
+
+//CONVERSÕES
+
+let numFloat =123.456;
+console.log(parseInt(numFloat));
+
+let numString ="123.456";
+console.log(parseFloat(numString));
+
+let numInt = 123456
+console.log(numInt.toString())
+
+//METODOS
+
+//length = verifica o tamanho da string
+let frase ="O mundo da tecnologia";
+console.log(frase.length);
+
+//indexOf -retorna um trecho do texto
+
+let texto="A programação indomável e sustentavel";
+console.log(texto.indexOf("ão"))
+
+console.log(texto.lastIndexOf("ão"))
+
+
+//slice - retorna um trecho do texto com inicio e final
+
+let info ="Processamento de ponta";
+console.log(info.slice(16,25))
+
+//OPERADORES ARITIMÉTICOS
+
+const num1=10;
+const num2=20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+
+//OPERADORES LÓGICOS
+
+const num3=20;
+const num4=30;
+
+console.log(num3 < num4);
+console.log(num3 > num4 && num4 < num3);
+console.log(num3 < num4 || num4 < num3); 
+
+//OPERADORES DE COMPARAÇÃO
+
+console.log(num3 == num4); // (float 30 == int 30 -> True)
+console.log(num3 === num4); //aqui o tipo e o valor tem que ser iguais (int 30 === int 30 -> True)
